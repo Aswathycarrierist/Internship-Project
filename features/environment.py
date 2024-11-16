f92f8107c9c175d0cad524e1666ce993441f9bdc
Anoop Pottammal <anooppottammal@Anoops-MBP.attlocal.net>
2024-11-16 12:41:38 -0500

    task cloud testing

--- features/environment.py
+++ features/environment.py
@@ -1,21 +1,24 @@
+from allure_behave.utils import scenario_name
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.support.wait import WebDriverWait
 from webdriver_manager.chrome import ChromeDriverManager
 from webdriver_manager.firefox import GeckoDriverManager
 from selenium.webdriver.firefox.service import Service
+from selenium.webdriver.chrome.options import Options
 
-
-
+# after browswer stack use pls remove scenario_name from browser init(context)
 from app.application import Application
-def browser_init(context):
+
+
+def browser_init(context, scenario_name):
     """
     :param context: Behave context
     """
     # driver_path = ChromeDriverManager().install()
     # service = Service(driver_path)
     # context.driver = webdriver.Chrome(service=service)
-    #chrome headless
+    # chrome headless
     # options = webdriver.ChromeOptions()
     # options.add_argument('headless')
     # service = Service(ChromeDriverManager().install())
@@ -23,21 +26,41 @@ def browser_init(context):
     #     options=options,
     #     service=service
     # )
-# firefox
+    # firefox
 
-    driver_path = GeckoDriverManager().install()
-    service = Service(driver_path)
-    context.driver = webdriver.Firefox(service=service)
+    # driver_path = GeckoDriverManager().install()
+    # service = Service(driver_path)
+    # context.driver = webdriver.Firefox(service=service)
+
+    # Browser stack
+    ### BROWSERSTACK ###
+    bs_user = 'aswathyvaliyapar_5uDAjq'
+    bs_key = 'hFM3GLqLXfszQMsRAfdo'
+    url = f'http://{bs_user}:{bs_key}@hub-cloud.browserstack.com/wd/hub'
+
+    options = Options()
+    bstack_options = {
+        "os": "Windows",
+        "osVersion": "11",
+        'browserName': 'chrome',
+        'sessionName': scenario_name
+    }
+    options.set_capability('bstack:options', bstack_options)
+    context.driver = webdriver.Remote(command_executor=url, options=options)
+
 
     context.driver.maximize_window()
     context.driver.implicitly_wait(4)
     context.driver.wait = WebDriverWait(context.driver, 10)
     context.app = Application(context.driver)
 
+
 def before_scenario(context, scenario):
     print('\nStarted scenario: ', scenario.name)
-    browser_init(context)
+    browser_init(context, scenario.name)
 
+
+# after brower stack you can remove last scenario.name
 
 def before_step(context, step):
     print('\nStarted step: ', step)
